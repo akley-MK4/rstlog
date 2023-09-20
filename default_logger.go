d48c782bee9b36bec254adb754aed6841efe03f7
wanghao <wanghao@casachina.com.cn>
2023-09-20 17:00:39 +0800

    Fix default logger display depth error

--- default_logger.go
+++ default_logger.go
@@ -48,68 +48,68 @@ func All(v ...interface{}) {
 	if LevelALL < defaultLogger.level {
 		return
 	}
-	defaultLogger.All(v...)
+	_ = defaultLogger.Output(LevelALL, "", v...)
 }
 
 func AllF(format string, v ...interface{}) {
 	if LevelALL < defaultLogger.level {
 		return
 	}
-	defaultLogger.AllF(format, v...)
+	_ = defaultLogger.Output(LevelALL, format, v...)
 }
 
 func Debug(v ...interface{}) {
 	if LevelDebug < defaultLogger.level {
 		return
 	}
-	defaultLogger.Debug(v...)
+	_ = defaultLogger.Output(LevelDebug, "", v...)
 }
 
 func DebugF(format string, v ...interface{}) {
 	if LevelDebug < defaultLogger.level {
 		return
 	}
-	defaultLogger.DebugF(format, v...)
+	_ = defaultLogger.Output(LevelDebug, format, v...)
 }
 
 func Info(v ...interface{}) {
 	if LevelInfo < defaultLogger.level {
 		return
 	}
-	defaultLogger.Info(v...)
+	_ = defaultLogger.Output(LevelInfo, "", v...)
 }
 
 func InfoF(format string, v ...interface{}) {
 	if LevelInfo < defaultLogger.level {
 		return
 	}
-	defaultLogger.InfoF(format, v...)
+	_ = defaultLogger.Output(LevelInfo, format, v...)
 }
 
 func Warning(v ...interface{}) {
 	if LevelWarning < defaultLogger.level {
 		return
 	}
-	defaultLogger.Warning(v...)
+	_ = defaultLogger.Output(LevelWarning, "", v...)
 }
 
 func WarningF(format string, v ...interface{}) {
 	if LevelWarning < defaultLogger.level {
 		return
 	}
-	defaultLogger.WarningF(format, v...)
+	_ = defaultLogger.Output(LevelWarning, format, v...)
 }
 
 func Error(v ...interface{}) {
 	if LevelError < defaultLogger.level {
 		return
 	}
-	defaultLogger.Error(v...)
+	_ = defaultLogger.Output(LevelError, "", v...)
 }
 
 func ErrorF(format string, v ...interface{}) {
 	if LevelError < defaultLogger.level {
 		return
 	}
-	defaultLogger.ErrorF(format, v...)
+	_ = defaultLogger.Output(LevelError, format, v...)
 }
